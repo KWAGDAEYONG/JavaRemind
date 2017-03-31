c287d0febee19ea1ed319979ee8063093fe41a24
kwagdaeyong <dayong7387@hanmail.net>
2017-03-31 16:38:42 +0900

    chatting

--- src/main/java/network/ServerExample.java
+++ src/main/java/network/ServerExample.java
@@ -1,6 +1,8 @@
 package network;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,6 +21,26 @@ public class ServerExample {
                 Socket socket = serverSocket.accept();
                 InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
                 System.out.println("[연결 수락]"+isa.getHostName());
+
+                byte[] bytes = null;
+                String message = null;
+
+                InputStream is = socket.getInputStream();
+                bytes = new byte[100];
+                int readByteCount = is.read(bytes);
+                message = new String(bytes, 0, readByteCount, "UTF-8");
+                System.out.println("[데이터 받기 성공]:"+message);
+
+                OutputStream os = socket.getOutputStream();
+                message = "Hello Client";
+                bytes = message.getBytes("UTF-8");
+                os.write(bytes);
+                os.flush();
+                System.out.println("[데이터 보내기 성공]");
+
+                is.close();
+                os.close();
+                socket.close();
             }
         }catch (Exception e){
 
